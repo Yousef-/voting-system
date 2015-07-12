2b00922279d64eb370b8d2ba850eb9e8a73025da
Yousef- <someproemail@gmail.com>
2015-07-12 03:13:52 -0500

    D.0.3
    changed the whole structure as it was way more complex than it needs to
    be

--- src/voting/driver.java
+++ src/voting/driver.java
@@ -1,22 +1,47 @@
 package voting;
 
+import java.util.*;
+
 public class Driver {
 	
-	static VotingModel vm = new VotingModel();
-	static VotingController vc = new VotingController();
-	static VotingView vv = new VotingView();
-
+	int candidates = 0;
+	int voters = 0;
+	int votes = 0;
+	
+	ArrayList<ArrayList<Integer>> votersA = new ArrayList<ArrayList<Integer>>();
+	ArrayList<ArrayList<Integer>> losersA = new ArrayList<ArrayList<Integer>>();
+	ArrayList<Integer> votesA = new ArrayList<Integer>();
 	
 	
+	Random rand = new Random();
 	
 	public static void main(String[] args)
 	{
-		vv.setModel(vm);
-		vc.setModel(vm);
-		vc.setView(vv);
 		
-		
-		vc.askForNumber();
 	}
-
+	
+	public void setArrays(int voterNum, int cansNum)
+	{
+		for(int i = 0; i < voterNum; i++)
+		{
+			this.votesA.clear();
+			for(int j = 0; j < cansNum-1; j++)
+			{
+				this.votesA.add(rand.nextInt(cansNum+1));
+			}
+			this.votersA.add(votesA);
+		}
+	}
+	
+	
+	public void addLosers()
+	{
+		
+	}
+	
+	public void checkVoter(int vote, int voter)
+	{
+		if(votersA.get(voter).get(vote).equals(0))
+			votersA.remove(voter);
+	}
 }
